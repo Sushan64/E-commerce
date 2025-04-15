be23d21a47c20ae3d16d76e91b6ec6353d02c1b1
Sushan Khatiwada <sushan.khatiwada2064@gmail.com>
2025-04-15 15:35:51 +0000

    Added Category

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=100)
+  parents = models.ForeignKey('Category', on_delete= models.CASCADE, null = True, blank = True, related_name ="SubCategory")
+
+  def __str__(self):
+    return self.name
+
+class Items(models.Model):
+  name = models.CharField(max_length = 200)
+  image = models.ImageField(upload_to="Pictures/")
+  description= models.TextField()
+  price = models.DecimalField(max_digits=30, decimal_places =3)
+  category = models.ManyToManyField(Category, related_name="Items")
+  
+
+  def __str__(self):
+    return self.name
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def home(request):
-  return render(request, 'ecommerce/home.html')
+  
+  men = models.Category.objects.get(name="Men")
+  clothes = models.Category.objects.get(name="Clothes")
+  
+  
+  
+  items = models.Items.objects.all()
+  category_cloth = models.Items.objects.filter(category = clothes)
+  category_men = models.Items.objects.filter(category = men)
+
+  context = {
+    'items': items,
+    'category_cloth': category_cloth,
+    'category_men': category_men,
+  }
+  
+  return render(request, 'ecommerce/home.html', context)
